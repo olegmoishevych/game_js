1d653877fee72e9b82b5b176a0c53675997865c1
olehmoishevych <olegmoishevych@gmail.com>
2024-10-05 16:18:40 +0300

    gravity

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 500 },
             debug: false
         }
     },
@@ -16,29 +16,112 @@ const config = {
     }
 };
 
-const cubes = [];
-
 const game = new Phaser.Game(config);
 
+let player;
+let cursors;
+let platforms;
+let stars;
+let score = 0;
+let scoreText;
+let canDoubleJump = false;
+
 function preload() {
-    this.load.image('cube', 'https://labs.phaser.io/assets/sprites/block.png');
+
+    this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
+    this.load.image('ground', 'https://labs.phaser.io/assets/sprites/platform.png');
+    this.load.image('star', 'https://labs.phaser.io/assets/demoscene/star.png');
+    this.load.spritesheet('dude',
+        'https://labs.phaser.io/assets/sprites/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
 
 function create() {
 
-    var ground = this.physics.add.staticGroup();
-    ground.create(400, 580, 'cube').setScale(8, 1).refreshBody();
+    this.add.image(400, 300, 'sky');
 
-    for (let i = 0; i < 50; i++) {
-        let cube = this.physics.add.image(Phaser.Math.Between(50, 750), Phaser.Math.Between(50, 300), 'cube');
-        cube.setBounce(0.6);
-        cubes.push(cube);
-    }
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 
-    this.physics.add.collider(cubes, ground);
-    this.physics.add.collider(cubes, cubes);
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player, platforms);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
 }
 
 function update() {
 
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+
+    if (player.body.touching.down) {
+        canDoubleJump = true;
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-500);
+    }
+
+    else if (cursors.up.isDown && canDoubleJump) {
+        player.setVelocityY(-450);
+        canDoubleJump = false;
+    }
+}
+
+function collectStar(player, star) {
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
 }
